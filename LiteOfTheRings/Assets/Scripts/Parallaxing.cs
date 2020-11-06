d335046001f456ecbd80efdb40f18440396f2121
Hajdú Bence <hajdu.benyo@gmail.com>
2020-11-06 15:43:22 +0100

    tiling started

--- LiteOfTheRings/Assets/Scripts/Parallaxing.cs
+++ LiteOfTheRings/Assets/Scripts/Parallaxing.cs
@@ -20,13 +20,14 @@ public class Parallaxing : MonoBehaviour
         float distance = (camera.transform.position.x * parallaxEffect);
         
         transform.position = new Vector3(startPosition + distance, transform.position.y, transform.position.z);
-
+        
+        /*
         if (temp > startPosition + length)
         {
             startPosition += length;
         } else if (temp < startPosition - length)
         {
             startPosition -= length;
-        }
+        } */
     }
 }
