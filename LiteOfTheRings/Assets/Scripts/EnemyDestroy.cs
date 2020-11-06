de0ad30862ed89b4944d7f45a4d6b3cc3bd0c8b3
Hajdú Bence <hajdu.benyo@gmail.com>
2020-11-06 14:24:50 +0100

    parallaxing done

--- LiteOfTheRings/Assets/Scripts/EnemyDestroy.cs
+++ LiteOfTheRings/Assets/Scripts/EnemyDestroy.cs
@@ -20,16 +20,4 @@ public class EnemyDestroy : MonoBehaviour
             }
         }
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
