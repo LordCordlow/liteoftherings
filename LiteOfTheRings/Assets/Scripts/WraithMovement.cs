d8b97f6841418e8e5a664a03cb6994ea2e6d752c
Hajdú Bence <hajdu.benyo@gmail.com>
2020-12-06 12:06:10 +0100

    pathfinding

---
+++ LiteOfTheRings/Assets/Scripts/WraithMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class WraithMovement : MonoBehaviour
+{
+    public AIPath aiPath;
+
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x >= 0.01f)
+        {
+            transform.localScale = new Vector3(2.5f, 2.5f, 1f);
+        } else if (aiPath.desiredVelocity.x <= 0.01f)
+        {
+            transform.localScale = new Vector3(-2.5f, 2.5f, 1f);
+        }
+    }
+}
